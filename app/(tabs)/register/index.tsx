090c7064359a095b1f92a7c650d1d70fba72893c
Ibrahim Imam <ibrhmmm552@gmail.com>
2024-05-20 21:30:04 +0700

    feat: register

---
+++ app/(tabs)/register/index.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Image, Text, View, SafeAreaView } from "react-native";
+
+export default function Register() {
+  return (
+    <SafeAreaView className=" flex-col justify-center items-center mt-36 ">
+      <Text className=" text-3xl font-bold">Register</Text>
+    </SafeAreaView>
+  );
+}
