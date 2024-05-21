ad8027ae53af57eaf6d0568c19d08f679e5ef69f
Ibrahim Imam <ibrhmmm552@gmail.com>
2024-05-21 19:19:01 +0700

    feat: campaign

--- app/(tabs)/project-detail/(content)/campaign.tsx
+++ app/(tabs)/project-detail/(content)/campaign.tsx
@@ -1,10 +1,53 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
+
+const dummy = [
+  {
+    url: "../../../../assets/images/banner.jpg",
+    desc: "Here’s a sneak-peek from the first chapter of CRYPTID.",
+  },
+  {
+    url: "../../../../assets/images/banner.jpg",
+    desc: "Here’s a sneak-peek from the first chapter of CRYPTID.",
+  },
+  {
+    url: "../../../../assets/images/banner.jpg",
+    desc: "Here’s a sneak-peek from the first chapter of CRYPTID.",
+  },
+  {
+    url: "../../../../assets/images/banner.jpg",
+    desc: "Here’s a sneak-peek from the first chapter of CRYPTID.",
+  },
+  {
+    url: "../../../../assets/images/banner.jpg",
+    desc: "Here’s a sneak-peek from the first chapter of CRYPTID.",
+  },
+];
 
 export default function Campaign() {
   return (
-    <View>
-      <Text>campaign</Text>
-    </View>
+    <ScrollView>
+      <View className=" px-5 pt-7 bg-white pb-12">
+        {dummy.map((data, index) => (
+          <View
+            key={index.toString()}
+            className="w-full mb-6 flex-col items-center justify-center"
+          >
+            <Image
+              source={require("../../../../assets/images/banner.jpg")}
+              style={{
+                resizeMode: "cover",
+                height: 300,
+                width: "100%",
+                borderRadius: 16,
+              }}
+            />
+            <Text className=" text-[#707181] mt-1">
+              Here’s a sneak-peek from the first chapter of CRYPTID.
+            </Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
