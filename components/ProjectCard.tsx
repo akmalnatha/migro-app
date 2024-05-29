646594333e200af473ed6326377a4a6e5469ea6e
akmalnatha <akmalabhirama2002@gmail.com>
2024-05-29 20:43:51 +0700

    refactor: project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -13,28 +13,26 @@ interface ProjectCardProps {
   bannerImageUrl: string;
   projectTitle: string;
   projectDesc: string;
-  organization: string;
+  owner: string;
   category: string;
   isWishlist: boolean;
   backers: number;
   daysToGo: number;
-  isProjectDetail: boolean;
-  isFeaturedProject: boolean;
-  isExploreCard: boolean;
+  type?: "featured" | "explore" | "explore-first";
+  isProjectDetail?: boolean;
 }
 
 export default function ProjectCard({
   bannerImageUrl,
   projectTitle,
   projectDesc,
-  organization,
+  owner,
   category,
   isWishlist,
   backers,
   daysToGo,
-  isProjectDetail,
-  isFeaturedProject,
-  isExploreCard,
+  type = "featured",
+  isProjectDetail = false,
 }: ProjectCardProps) {
   const [wishlist, setWishlist] = useState(isWishlist);
 
@@ -42,8 +40,7 @@ export default function ProjectCard({
     setWishlist(!wishlist);
   };
   return isProjectDetail ? (
-    // Project Detail
-    <View className="">
+    <View className=" m-3 mt-11">
       <Image
         source={require("../assets/images/banner.jpg")}
         className=" w-full rounded-2xl h-52 object-cover"
@@ -60,7 +57,7 @@ export default function ProjectCard({
             ellipsizeMode="tail"
             className=" text-black ml-2 mt-4 text-xs"
           >
-            {organization}
+            {owner}
           </Text>
         </View>
       </View>
@@ -87,47 +84,73 @@ export default function ProjectCard({
         </View>
       </View>
     </View>
-  ) : isFeaturedProject ? (
-    // Featured Project Card di HOME PAGE dan EXPLORE PAGE
-    <View style={styles.featuredProjectCard}>
-      <View style={styles.imageContainer}>
+  ) : (
+    <View
+      style={{ shadowColor: "#000000", shadowOpacity: 0.25, shadowRadius: 4 }}
+      className={`bg-[#F9FAF5] rounded-[16px] flex ${
+        type == "explore" ? "flex-row" : "flex-col"
+      }`}
+    >
+      <View className={`${type == "explore" ? "w-[35%]" : "w-full"} relative`}>
         <Image
           source={require("../assets/images/banner.jpg")}
-          style={styles.banner}
+          className={`w-full ${
+            type == "explore"
+              ? "h-[100px] rounded-l-[16px]"
+              : "h-[140px] rounded-t-[16px]"
+          } object-cover`}
         />
         <TouchableOpacity
-          style={styles.wishlist}
+          className={`rounded-full absolute ${
+            type == "explore" ? "top-2 left-2" : "top-3 right-3"
+          } bg-white p-1`}
           onPress={handleWishlistToggle}
         >
           <Ionicons
             name={wishlist ? "heart" : "heart-outline"}
-            size={24}
+            size={type == "explore" ? 20 : 24}
             color={wishlist ? "red" : "black"}
           />
         </TouchableOpacity>
-        <View className=" bg-[#49CCB4] rounded-xl flex-row items-center justify-start absolute right-2 bottom-3 p-1">
-          <Ionicons name={"time-outline"} size={24} color={"white"} />
-          <Text className=" text-white ml-1">{daysToGo} day(s)</Text>
+        <View
+          className={` ${
+            type == "featured"
+              ? "hidden"
+              : type == "explore-first"
+              ? "bottom-3 right-3"
+              : "block bottom-2 left-2"
+          } bg-[#49CCB4] rounded-[4px] flex-row items-center justify-start absolute bottom-3 p-1`}
+        >
+          <Ionicons name={"time-outline"} size={16} color={"white"} />
+          <Text className=" text-white text-[12px] ml-1">
+            {daysToGo} day(s)
+          </Text>
         </View>
       </View>
-      <View className=" flex-row items-center justify-between mt-4 mb-2 mx-3 flex-wrap">
-        <View className=" flex-row items-center justify-start">
-          <Text className=" text-black font-bold text-xl">{projectTitle}</Text>
+      <View
+        className={`${type == "explore" ? "grow" : "w-full"} flex gap-3 p-3`}
+      >
+        <View className="flex-row items-center justify-between flex-wrap">
+          <Text className=" text-black font-bold text-[20px]">
+            {projectTitle}
+          </Text>
+          <View className=" flex-row items-center justify-start gap-2">
+            <Ionicons
+              name={"pricetag-outline"}
+              size={20}
+              color={"black"}
+              style={{ transform: [{ scaleX: -1 }] }}
+            />
+            <Text>{category}</Text>
+          </View>
         </View>
-        <View className=" flex-row items-center justify-start gap-2">
-          <Ionicons
-            name={"pricetag-outline"}
-            size={20}
-            color={"black"}
-            style={styles.categoryIcon}
-          />
-          <Text>{category}</Text>
-        </View>
-      </View>
-      {!isExploreCard ? <Text className=" mx-3">{organization}</Text> : <></>}
-      {/* Progress Bar Indicator */}
-      {!isExploreCard ? (
-        <View className="flex-row items-center justify-start my-3 mx-3">
+        {type != "explore" ? <Text className=" mx-3">{owner}</Text> : <></>}
+        {/* Progress Bar Indicator */}
+        <View
+          className={`${
+            type == "featured" ? "flex" : "hidden"
+          } flex-row items-center justify-start`}
+        >
           <Image
             className="w-5 h-5 mr-2"
             source={require("../assets/images/backers-icon.png")}
@@ -139,112 +162,7 @@ export default function ProjectCard({
           />
           <Text>{daysToGo} day(s) to go</Text>
         </View>
-      ) : (
-        <></>
-      )}
-    </View>
-  ) : (
-    <View className=" w-full bg-white rounded-2xl flex-row overflow-hidden pr-2">
-      <View className=" relative">
-        <Image
-          source={require("../assets/images/banner.jpg")}
-          style={styles.cardImage}
-        />
-        <TouchableOpacity
-          onPress={handleWishlistToggle}
-          style={styles.wishlistCard}
-        >
-          <Ionicons
-            // style={styles.wishlistCard}
-            name={wishlist ? "heart" : "heart-outline"}
-            size={20}
-            color={wishlist ? "red" : "black"}
-          />
-        </TouchableOpacity>
-        {/* Progress Bar Indicator */}
-        <View className=" bg-[#49CCB4] rounded-xl flex-row items-center justify-start absolute left-2 bottom-3 p-1">
-          {/* Progress Bar Indicator */}
-          <Ionicons name={"time-outline"} size={18} color={"white"} />
-          <Text className=" text-white ml-1 text-[10px]">
-            {daysToGo} day(s)
-          </Text>
-        </View>
-      </View>
-      {/* elipsis */}
-      <View className=" ml-2 flex-col flex-1 py-3">
-        <Text
-          numberOfLines={1}
-          ellipsizeMode="tail"
-          className=" font-bold text-base"
-        >
-          {projectTitle}
-        </Text>
-        <View className=" flex-row items-center justify-start gap-2 mb-6">
-          <Ionicons
-            name={"pricetag-outline"}
-            size={20}
-            color={"#707181"}
-            style={styles.categoryIcon}
-          />
-          <Text className=" text-gray-500 ">{category}</Text>
-        </View>
-        <View className=" bg-[#49CCB4] h-2 w-full"></View>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  featuredProjectCard: {
-    width: "100%",
-    backgroundColor: "white",
-    borderWidth: 1,
-    borderColor: "#D9D9D9",
-    paddingBottom: 8,
-    borderRadius: 18,
-    shadowColor: "#000000",
-    shadowOffset: { width: 0, height: -30 },
-    shadowOpacity: 0.25,
-    shadowRadius: 50,
-  },
-  imageContainer: {
-    position: "relative",
-  },
-  banner: {
-    width: "100%",
-    borderTopLeftRadius: 16,
-    borderTopRightRadius: 16,
-    height: 160,
-    resizeMode: "cover",
-  },
-  wishlist: {
-    position: "absolute",
-    top: 12,
-    right: 12,
-    backgroundColor: "white",
-    borderRadius: 9999,
-    padding: 4,
-  },
-  wishlistCard: {
-    position: "absolute",
-    top: 12,
-    left: 12,
-    backgroundColor: "white",
-    borderRadius: 9999,
-    padding: 4,
-  },
-  cardImage: {
-    width: 124,
-    height: 108,
-    borderBottomLeftRadius: 16,
-    borderTopLeftRadius: 16,
-  },
-  text: {
-    color: "black",
-    marginLeft: 8,
-    marginTop: 16,
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-  categoryIcon: { transform: [{ scaleX: -1 }] },
-});
