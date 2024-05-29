44c21538963d6714eff60ff48d0c686f4603df45
Ibrahim Imam <ibrhmmm552@gmail.com>
2024-05-29 16:55:09 +0700

    feat: update login register

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -39,70 +39,76 @@ export default function Login() {
   }
 
   return (
-    <SafeAreaView className=" justify-center items-center h-full px-5 bg-[#F9F9F9]">
-      <Image
-        source={require("@/assets/images/migro-text.png")}
-        className="w-52 h-20 mb-4"
-        style={{ resizeMode: "contain" }}
-      />
-      <Text className=" text-center font-bold text-xl mb-14">
-        {"Dreams Funded,\n Rewards Unbounded"}
-      </Text>
-      <TextInput
-        placeholder="Email or Username"
-        onChangeText={onChangeEmail}
-        value={email}
-        className=" border p-3 w-full h-12 rounded-md mb-4 bg-white"
-      ></TextInput>
-      <View className=" border p-3 w-full h-12 rounded-md relative bg-white">
-        <TextInput
-          secureTextEntry={obscure}
-          placeholder="Password"
-          onChangeText={onChangePassword}
-          value={password}
-          className=" w-10/12"
-        ></TextInput>
-        <View className=" absolute right-5 top-3">
-          <Pressable
-            onPress={() => {
-              onChangeObscure(!obscure);
-            }}
+    <>
+      <SafeAreaView className=" justify-center items-center h-full bg-white">
+        <View className="w-full bg-white px-5 flex-col justify-center items-center z-10 pb-10">
+          <Image
+            source={require("@/assets/images/migro-text.png")}
+            className="w-52 h-20 mb-4"
+            style={{ resizeMode: "contain" }}
+          />
+          <Text className=" text-center font-bold text-xl mb-14">
+            {"Dreams Funded,\n Rewards Unbounded"}
+          </Text>
+          <TextInput
+            placeholder="Email or Username"
+            onChangeText={onChangeEmail}
+            value={email}
+            className=" border p-3 w-full h-12 rounded-md mb-4 bg-white"
+          ></TextInput>
+          <View className=" border p-3 w-full h-12 rounded-md relative bg-white">
+            <TextInput
+              secureTextEntry={obscure}
+              placeholder="Password"
+              onChangeText={onChangePassword}
+              value={password}
+              className=" w-10/12"
+            ></TextInput>
+            <View className=" absolute right-5 top-3">
+              <Pressable
+                onPress={() => {
+                  onChangeObscure(!obscure);
+                }}
+              >
+                <Ionicons
+                  name={obscure ? "eye-off" : "eye"}
+                  size={20}
+                  color={"black"}
+                />
+              </Pressable>
+            </View>
+          </View>
+          <Text className=" mt-2 text-right w-full text-[#2E3362]">
+            Forgot your password?
+          </Text>
+          <TouchableOpacity
+            className={`w-full p-3 rounded-3xl items-center justify-center mt-10 ${
+              loading ? "bg-[#707181]" : "bg-[#008E8A]"
+            }`}
+            disabled={loading}
+            onPress={() => signInWithEmail()}
           >
-            <Ionicons
-              name={obscure ? "eye-off" : "eye"}
-              size={20}
-              color={"black"}
-            />
-          </Pressable>
+            {loading ? (
+              <ActivityIndicator size="small" color="white" />
+            ) : (
+              <Text className=" text-white">Login</Text>
+            )}
+          </TouchableOpacity>
+          <View className=" flex-row mt-5">
+            <Text className=" text-sm">Don’t have an account?</Text>
+            <Pressable onPress={() => router.replace("/(auth)/register")}>
+              <Text className=" text-sm text-[#2E3362] font-bold ml-1 underline">
+                Register here
+              </Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
-      <Text className=" mt-2 text-right w-full text-[#2E3362]">
-        Forgot your password?
-      </Text>
-      <TouchableOpacity
-        className={`w-full p-3 rounded-3xl items-center justify-center mt-10 ${
-          loading ? "bg-[#707181]" : "bg-[#008E8A]"
-        }`}
-        disabled={loading}
-        onPress={() => signInWithEmail()}
-      >
-        {loading ? (
-          <ActivityIndicator size="small" color="white" />
-        ) : (
-          <Text className=" text-white">Login</Text>
-        )}
-      </TouchableOpacity>
-      <View className=" flex-row mt-5">
-        <Text className=" text-sm">Don’t have an account?</Text>
-        <Text className=" text-sm text-[#2E3362] font-bold ml-1 underline">
-          Register here
-        </Text>
-      </View>
-      {/* <Image
-        source={require("../../../assets/images/bottom-login.png")}
-        className="w-52 h-20 mb-4"
-        style={{ resizeMode: "contain" }}
-      /> */}
-    </SafeAreaView>
+        <Image
+          source={require("../../assets/images/bottom-login.png")}
+          className="w-full h-[125px] absolute bottom-0"
+          style={{ resizeMode: "contain" }}
+        />
+      </SafeAreaView>
+    </>
   );
 }
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,5 +1,6 @@
 import { supabase } from "@/lib/supabase";
 import Ionicons from "@expo/vector-icons/build/Ionicons";
+import { router } from "expo-router";
 import { useState } from "react";
 import {
   StyleSheet,
@@ -95,67 +96,80 @@ export default function Register() {
   }
 
   return (
-    <SafeAreaView className=" flex-col justify-center items-center px-5 bg-[#F9F9F9] h-full">
-      <Text className=" text-3xl font-bold mb-8">Register</Text>
-      <TextInput
-        placeholder="Full Name"
-        onChangeText={onChangeFullname}
-        value={fullname}
-        className=" border p-3 w-full h-12 rounded-md mb-4 bg-white"
-      ></TextInput>
-      <TextInput
-        placeholder="Username"
-        onChangeText={onChangeUsername}
-        value={username}
-        className=" border p-3 w-full h-12 rounded-md mb-4 bg-white"
-      ></TextInput>
-      <TextInput
-        placeholder="Email"
-        onChangeText={onChangeEmail}
-        value={email}
-        className=" border p-3 w-full h-12 rounded-md mb-4  bg-white"
-      ></TextInput>
-      <View className=" border p-3 w-full h-12 rounded-md relative  bg-white">
+    <SafeAreaView className=" flex-col justify-center items-center bg-[#F9F9F9] h-full">
+      <Text className=" text-3xl font-bold mb-8  px-5">Register</Text>
+      <View className="w-full px-5 flex-col justify-center items-center">
         <TextInput
-          secureTextEntry={obscure}
-          placeholder="Password"
-          onChangeText={onChangePassword}
-          value={password}
-          className=" w-10/12 "
+          placeholder="Full Name"
+          onChangeText={onChangeFullname}
+          value={fullname}
+          className=" border p-3 w-full h-12 rounded-md mb-4 bg-white"
         ></TextInput>
-        <View className=" absolute right-5 top-3">
+        <TextInput
+          placeholder="Username"
+          onChangeText={onChangeUsername}
+          value={username}
+          className=" border p-3 w-full h-12 rounded-md mb-4 bg-white"
+        ></TextInput>
+        <TextInput
+          placeholder="Email"
+          onChangeText={onChangeEmail}
+          value={email}
+          className=" border p-3 w-full h-12 rounded-md mb-4  bg-white"
+        ></TextInput>
+        <View className=" border p-3 w-full h-12 rounded-md relative  bg-white">
+          <TextInput
+            secureTextEntry={obscure}
+            placeholder="Password"
+            onChangeText={onChangePassword}
+            value={password}
+            className=" w-10/12 "
+          ></TextInput>
+          <View className=" absolute right-5 top-3">
+            <Pressable
+              onPress={() => {
+                onChangeObscure(!obscure);
+              }}
+            >
+              <Ionicons
+                name={obscure ? "eye-off" : "eye"}
+                size={20}
+                color={"black"}
+              />
+            </Pressable>
+          </View>
+        </View>
+        <TouchableOpacity
+          className={`w-full p-3 rounded-3xl items-center justify-center mt-10 ${
+            loading ? "bg-[#707181]" : "bg-[#008E8A]"
+          }`}
+          disabled={loading}
+          onPress={() => signUpWithEmail()}
+        >
+          {loading ? (
+            <ActivityIndicator size="small" color="white" />
+          ) : (
+            <Text className=" text-white">Register</Text>
+          )}
+        </TouchableOpacity>
+        <View className=" flex-row mt-5">
+          <Text className=" text-sm">Have an account?</Text>
           <Pressable
             onPress={() => {
-              onChangeObscure(!obscure);
+              router.replace("/(auth)");
             }}
           >
-            <Ionicons
-              name={obscure ? "eye-off" : "eye"}
-              size={20}
-              color={"black"}
-            />
+            <Text className=" text-sm text-[#2E3362] font-bold ml-1 underline">
+              Login
+            </Text>
           </Pressable>
         </View>
       </View>
-      <TouchableOpacity
-        className={`w-full p-3 rounded-3xl items-center justify-center mt-10 ${
-          loading ? "bg-[#707181]" : "bg-[#008E8A]"
-        }`}
-        disabled={loading}
-        onPress={() => signUpWithEmail()}
-      >
-        {loading ? (
-          <ActivityIndicator size="small" color="white" />
-        ) : (
-          <Text className=" text-white">Register</Text>
-        )}
-      </TouchableOpacity>
-      <View className=" flex-row mt-5">
-        <Text className=" text-sm">Have an account?</Text>
-        <Text className=" text-sm text-[#2E3362] font-bold ml-1 underline">
-          Login
-        </Text>
-      </View>
+      {/* <Image
+        source={require("../../assets/images/bottom-login.png")}
+        className="w-full h-[125px] absolute bottom-0"
+        style={{ resizeMode: "contain" }}
+      /> */}
     </SafeAreaView>
   );
 }
